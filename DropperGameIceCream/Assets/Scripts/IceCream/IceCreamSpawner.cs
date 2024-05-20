a1aa26bd83bdf0aeac2404dc5f9ec42fcb5c3859
Markypin <markypin@gmail.com>
2024-05-20 04:34:01 -0600

    Button Fixed, Random scoops
    Button now works upon being clicked, meaning clicks at random places don't spawn ice cream anymore, sidd's code was reworked so random ice creams scoops spawn. Animation for the button was also tweaked.

--- DropperGameIceCream/Assets/Scripts/IceCream/IceCreamSpawner.cs
+++ DropperGameIceCream/Assets/Scripts/IceCream/IceCreamSpawner.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class IceCreamSpawner : MonoBehaviour
+
+public class IcecreamSpawner : MonoBehaviour
 {
-    public GameObject ScoopIceCreamPrefab;
-   
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private GameObject[] iceScoops;
+    public Animator click;
+    
+
+    void OnMouseDown () //This method allows the ice scoops to spawn UPON pressing the button, and not at random places
     {
-        if (Input.GetMouseButtonDown(0))
+
+        if (Input.GetMouseButtonDown(0))       
         {
-            Instantiate(ScoopIceCreamPrefab,transform.position,Quaternion.identity);
+            
+            click.SetTrigger("Click");
+            int randomScoopSpawn = Random.Range(0, 6);  //random range for the balls of icream prefabs
+
+            Instantiate(iceScoops[randomScoopSpawn], new Vector3(4.91f, -0.11f, 0f), Quaternion.identity);  //the icecream scoops will spawn at the spawner's location, which is nowhere near the button, that's why in has specific coordinates. **the vector3 numbers**
         }
-           
     }
 }
